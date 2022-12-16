c96a632c158f26f957771ecc2640c2950271677a
kid3234 <117932245+kid3234@users.noreply.github.com>
2022-12-16 11:57:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+const inputEl = document.getElementById("input-el")
+const pleEl = document.getElementById("ple-el")
+const pvoEl = document.getElementById("pvo-el")
+const pmaEl = document.getElementById("pma-el")
+// const feet = (Number(inputEl.value) * 3.281).toFixed()
+// const meter = (Number(inputEl.value) / 3.281).toFixed()
 
-function increment() {
-    count += 1
-    countEl.textContent = count
-}
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
-}
+ const btn = document.getElementById("btn-el")
+btn.addEventListener("click",function(){
+    pleEl.textContent = `${inputEl.value} meters 
+    = ${(Number(inputEl.value) * 3.281).toFixed(3)} feet |
+     ${inputEl.value} feets = ${(Number(inputEl.value) / 3.281).toFixed(3)} meters`
+     
+     pvoEl.textContent = `${inputEl.value} liters 
+    = ${(Number(inputEl.value) * 0.264).toFixed(3)} gallons |
+     ${inputEl.value} gallons = ${(Number(inputEl.value) / 0.264).toFixed(3)} liters `
+     
+     pmaEl.textContent = `${inputEl.value} kilos
+    = ${(Number(inputEl.value) * 2.204).toFixed(3)} pounds |
+     ${inputEl.value} pounds = ${(Number(inputEl.value) / 2.204).toFixed(3)} kilos `
+     
+     
+})
+
+ 
+  
